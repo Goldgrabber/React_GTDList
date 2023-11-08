99a491241a5d2294d63020ff5970731822460712
Goldgrabber <goldgraber@gmail.com>
2023-11-09 00:09:00 +0300

    css-1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import listSvg from './assets/img/list.svg';
 
 
 function App() {
-  return (
-    <h1>123</h1>
-  );
+  return <div className='todo'>
+    <div className="todo__sidebar">
+        <div className="todo__list">
+          <li>
+            <i>
+              <img src={listSvg} alt="List icon"/>
+            </i>
+            <span> Все задачи </span>
+          </li>
+        </div>
+    </div>
+    <div className="todo__TASKS">
+
+    </div>
+  
+
+  </div>;
 }
 
 export default App;
