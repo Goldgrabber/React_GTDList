2a19e4143b7936b3f254991da94de35299843be4
Goldgrabber <goldgraber@gmail.com>
2023-11-16 10:50:53 +0300

    layout-3

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import './List.scss'
 
-const List = ({ items }) => {
-    return    (    
+const List = ({ items,  }) => {
+    return (    
     <ul className="list">
-      {
-        items.map(item => <li>
+      {items.map( (item, index) => (
+        <li key={index} className={item.active ? 'active': ''}>
           <i> 
             {item.icon ? ( 
             item.icon
@@ -15,8 +16,8 @@ const List = ({ items }) => {
           )}
           </i>
           <span> {item.name} </span>
-        </li>)
-      }
+        </li>
+      ))}
     
   </ul>
     )
