0dbef5598b897f1f44575f61fb8ee200d8b0e7f4
Goldgrabber <goldgraber@gmail.com>
2023-12-10 14:29:13 +0300

    layout-5

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import listSvg from './assets/img/list.svg';
+import { useState } from 'react';
 import List from './components/List';
+import AddListButton from './components/AddListButton';
 
 
 function App() {
+  let [value, setValue] = useState('Hello, World!');
+
   return <div className='todo'>
     <div className="todo__sidebar">
       <List 
@@ -36,21 +39,8 @@ function App() {
       isRemovable
       />
 
-<List 
-        items={[
-          {
-            className: "List__add-button",
-            icon: <svg width="12" height="12" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M8 1V15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M1 8H15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>
-            ,
-            name: "Добавить список"
-          },
+      <AddListButton/>
 
-        ]}
-      isRemovable
-      />
     </div>
     <div className="todo__TASKS">
 
